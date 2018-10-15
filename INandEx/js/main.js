b2186d794b645298b7eec149540550be4748f120
pc <pc@KAPILZ>
2018-10-15 11:42:41 +0530

    changed controllers

--- INandEx/js/main.js
+++ INandEx/js/main.js
@@ -2,10 +2,17 @@ var inExApp = angular.module('inExApp', ['ngRoute', 'ngCookies']);
 
 inExApp.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-   .when('/', {
-       controller: 'landingController',
-       templateUrl: 'components/landing/landing.html',
-       controllerAs: 'vm'
-   })
-   .otherwise({ redirectTo: 'components/landing/landing.html' });
+        .when('/', {
+            templateUrl: 'components/login/login.html',
+            controller: 'loginController'
+        })
+        .when('/dashboard', {
+            templateUrl: 'components/dashboard/dashboard.html',
+            controller: 'dashboardController'
+        })
+        .when('/addExpense', {
+            templateUrl: 'components/addExpense/addExpense.html',
+            controller: 'addExpenseController'
+        })
+        .otherwise({ redirectTo: 'components/login/login.html' });
 }])
